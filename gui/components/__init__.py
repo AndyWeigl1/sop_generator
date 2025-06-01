c40ec96fc5f53024653b0ad30d67f6fed4022654
AndyWeigl1 <140345538+AndyWeigl1@users.noreply.github.com>
2025-06-01 13:39:19 -0600

    Create __init__.py

---
+++ gui/components/__init__.py
@@ -0,0 +1,10 @@
+# gui/components/__init__.py
+"""
+GUI Components Package
+
+This package contains reusable UI components for the SOP Builder.
+"""
+
+from .text_formatting_toolbar import TextFormattingToolbar, ToolTip
+
+__all__ = ['TextFormattingToolbar', 'ToolTip']
